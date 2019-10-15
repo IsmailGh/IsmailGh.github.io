e5750a848d4e726a0e30008a3ae0f00672e04a36
Ismail Ghelle <ismailghelle6@gmail.com>
2019-10-15 19:32:24 -0400

    Updated UI

---
+++ src/components/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./Home.css"
+import { Spring } from 'react-spring/renderprops'
+
+function Home(props) {
+    return(
+        <div class="wrapper">
+            <section class="top">
+                <div class="top__content">
+                    <Spring from={{opacity:0 , marginTop:-500}}
+                    to={{opacity:1, marginTop:0}} 
+                    config={{delay:500,duration:700}}
+                    >
+                        {props => (
+                            <div style={props}>
+                                <span class='resume'>
+                                    <a href="/resume.pdf" target="_blank" rel="nofollow noopener noreferrer" 
+                                    >Resume</a>
+                                </span>
+                                <p class="greet">Hi, My name is <br />
+                                </p>
+                                <h1>
+                                <span class="name">
+                                Ismail Ghelle
+                                </span> <br />
+                                <span class="description">
+                                I build things on the web.
+                                </span><br />
+                                <span class="underline">
+                                </span>
+                                </h1>
+                                <p class="para">
+                                I’m a software engineer based in Toronto, currently studying at Ryerson University. I enjoy building and designing great, high-quality websites and applications. 
+                                </p>
+                                <span class="button">
+                                <a href="mailto:ismailghelle6@gmail.com">Get in touch!</a>
+                                </span>
+                            </div>
+                        )}
+                    </Spring>
+                </div>
+            </section>
+         </div>      
+    )
+}
+
+export default Home
